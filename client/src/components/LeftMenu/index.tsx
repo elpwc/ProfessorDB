d20a572d386af31d4e76b2bfab1b25351ee416ec
elpwc/wniko <elpwc@hotmail.com>
2022-06-08 16:19:16 +0800

    feat: left menu

---
+++ client/src/components/LeftMenu/index.tsx
@@ -0,0 +1,68 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+
+export default () => {
+	const mylocation = useLocation();
+
+	const menu = [
+		{
+			icon: (
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+					<path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
+				</svg>
+			),
+			title: "首页",
+			to: "",
+			route: "",
+		},
+		{
+			icon: (
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+				</svg>
+			),
+			title: "学校",
+			to: "",
+			route: "universities",
+		},
+		{
+			icon: (
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+				</svg>
+			),
+			title: "专攻",
+			to: "",
+			route: "subjects",
+		},
+		{
+			icon: (
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+					<path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z" />
+				</svg>
+			),
+			title: "关于",
+			to: "",
+			route: "about",
+		},
+	];
+
+  console.log(mylocation.pathname.split("/"))
+
+	return (
+		<div className="menuContainer">
+			<nav className="leftnav">
+				{menu.map((menuitem) => {
+					return (
+						<Link to={menuitem.to} className={"navitemContainer" + (mylocation.pathname.split("/").pop() === menuitem.route ? " checked" : "")}>
+							<p className="navitem">
+								<span>{menuitem.icon}</span>
+								<span>{menuitem.title}</span>
+							</p>
+						</Link>
+					);
+				})}
+			</nav>
+		</div>
+	);
+};
--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -4,6 +4,7 @@ import { Row, Col } from "antd";
 import "./index.css";
 import { Header } from "antd/lib/layout/layout";
 import { Link } from "react-router-dom";
+import LeftMenu from "../../components/LeftMenu";
 
 interface P {}
 
@@ -11,6 +12,8 @@ export default (props: P) => {
 	const params = useParams();
 	const navigate = useNavigate();
 	const mylocation = useLocation();
+
+	const [showLeft, setshowLeft]: [boolean, any] = useState(true);
 
 	// let currentId: string = params.id as string;
 
@@ -21,13 +24,15 @@ export default (props: P) => {
 	return (
 		<>
 			<header>
-				<Link to="">
-					<button>
-						<svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" viewBox="0 0 16 16">
-							<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-						</svg>
-					</button>
-				</Link>
+				<button
+					onClick={() => {
+						setshowLeft(!showLeft);
+					}}
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" viewBox="0 0 16 16">
+						<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+					</svg>
+				</button>
 
 				<Link to="/">
 					<p>Professor DB</p>
@@ -41,7 +46,20 @@ export default (props: P) => {
 					</button>
 				</Link>
 			</header>
-			<Outlet />
+
+			<Row style={{ height: "100%" }}>
+				{showLeft ? (
+					<Col span={4}>
+						<LeftMenu />
+					</Col>
+				) : (
+					<></>
+				)}
+				
+				<Col span={showLeft ? 20 : 24}>
+					<Outlet />
+				</Col>
+			</Row>
 		</>
 	);
 };
