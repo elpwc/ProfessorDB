360a7c12847935a994d169ed2bb048c585585673
elpwc/wniko <elpwc@hotmail.com>
2022-06-10 03:06:12 +0800

    server: user

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  @ApiProperty()
+  id: number;
+
+  @Column({ length: 64, nullable: false })
+  @ApiProperty()
+  username: string;
+
+  @Column({ length: 64, nullable: false })
+  @ApiProperty()
+  password: string;
+
+  @Column('datetime', { default: () => 'CURRENT_TIMESTAMP' })
+  @ApiProperty()
+  lastlogin: Date;
+
+  @Column('datetime', { default: () => 'CURRENT_TIMESTAMP' })
+  @ApiProperty()
+  createtime: Date;
+}
