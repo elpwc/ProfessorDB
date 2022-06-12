268261f0767e2a2bd6d745cc5a5b4cd8f53e8759
elpwc <elpwc@hotmail.com>
2022-06-12 20:05:32 +0800

    feat: testdata, consider to use mock

---
+++ client/src/utils/testdata.ts
@@ -0,0 +1,36 @@
+export const TestProfessors = [
+    {
+        id: 114514,
+        namejp: '田所浩二',
+        university: '下北泽农林技术学院大学',
+        subject: '茶业',
+        direction: '红茶栽培与冲调',
+        country: '日本',
+        canSpeakChinese: false,
+        photo: '',
+        email: 'tadokorokouji_ochadaisuki@shimokitazawa.jp',
+        website: 'http://chagyou.shimokitazawauniversity.edu.jp/koucha/?professor=tadokorokouji',
+        recommendation: 1,
+        haiwang: 5,
+        replyrate: 2,
+        description: `
+        1145141919810
+        まず年齢を教えてくれないかな
+        24歳です
+        24歳...もう働いてんの？
+        学生です
+        学生...(軽蔑)身長体重どれぐらいあんの？
+        身長は１７０ｃｍで、体重は７４ｋｇです
+        体がっちりしてるよね、スポーツとかやってんの？
+        スポーツはやってないっすけど、トレニンーグはやってます
+        じゃあ、オナニーとかっていうのは？
+        やりますねぇ！
+        `,
+        mine: `
+        学生を家に連れて、薬入れた紅茶で昏睡状態にさせて、レープする噂がある
+        `,
+        students: '无',
+        
+
+    } as API.Professor
+];
