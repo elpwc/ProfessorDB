c1e4f5d48a1f467bb9d9091b417c938c11e568c1
elpwc/wniko <elpwc@hotmail.com>
2022-06-15 14:10:57 +0800

    feat: add university page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './pages/About';
 import Add from './pages/AddProfessor';
+import AddUniversity from './pages/AddUniversity';
 import ErrorPage from './pages/ErrorPage';
 import Home from './pages/Home';
 import Main from './pages/Main';
@@ -15,6 +16,7 @@ function App() {
         <Route path="" element={<Main />}>
           <Route index element={<Home />}></Route>
           <Route path="add" element={<Add />}></Route>
+          <Route path="adduni" element={<AddUniversity />}></Route>
           <Route path="about" element={<About />}></Route>
           <Route path="test" element={<TestPage />}></Route>
           <Route path="professor" element={<Home />}></Route>
